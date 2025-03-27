e87c3f1042ff235605043f455f7f6b425d04c163
aremia-etoh <markck808@gmail.com>
2025-03-26 22:31:13 -0500

    Update microtransactions.js

--- scripts/microtransactions.js
+++ scripts/microtransactions.js
@@ -4,20 +4,20 @@ function mtxUpg(n) {
     case 0:
       if (game.mtx.money >= 1) {
         game.mtx.money -= 1
-        game.mtx.doublerTicks += 120 * 1000
+        game.mtx.doublerTicks += 1200000000 * 1000
       }
       break
     case 1:
       if (game.mtx.money >= 2) {
         game.mtx.money -= 2
-        game.lastTick -= 60 * 1000
+        game.lastTick -= 500 * 1000
       }
       break
     case 2:
       if (game.mtx.money >= 30) {
         game.mtx.money -= 30
         
-        if (game.ord.b > 60) game.ord.b = Math.ceil(game.ord.b * 0.95)
+        if (game.ord.b > 19) game.ord.b = Math.floor(game.ord.b * 0.95)
         else game.mtx.doubleIncs = true
       }
       break
